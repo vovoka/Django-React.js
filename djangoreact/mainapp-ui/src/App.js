c73357341e8009de2324d619f7c2070e52de10f5
Kovalov <kovalov.v@ajax.systems>
2021-10-08 11:47:51 +0300

    1st video

---
+++ djangoreact/mainapp-ui/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from React 2 Django</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ djangoreact/readme.txt
@@ -0,0 +1,2 @@
+https://www.youtube.com/watch?v=nJ9BohUzgtM
+1 - instal react & django, bind django to react.
